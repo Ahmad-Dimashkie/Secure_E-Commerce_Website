ef3481bc0140b1452a5ed72cd2add9d1f1bdf674
Ahmad Dimashkie <afd08@mail.aub.edu>
2024-11-10 16:17:02 +0200

    test

--- user_and_role_management/config.py
+++ user_and_role_management/config.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = 900  # 15 minutes
