a9014257426285b246903b4dc10de8038b6c61bf
Ahmad Dimashkie <afd08@mail.aub.edu>
2024-11-10 13:10:08 +0200

    added sign in page

--- frontend_ecommerce/src/App.js
+++ frontend_ecommerce/src/App.js
@@ -1,16 +1,18 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
-import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
-import AdminDashboard from './pages/AdminDashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AdminDashboard from "./pages/AdminDashboard";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/products/:id" element={<ProductDetails />} />
