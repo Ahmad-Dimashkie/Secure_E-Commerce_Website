62ea416cab4627c3608860feeddcdc44ce225c56
Ahmad Dimashkie <afd08@mail.aub.edu>
2024-11-15 11:26:31 +0200

    i will need to remove everythin related to csrf

--- frontend_ecommerce/src/services/api.js
+++ frontend_ecommerce/src/services/api.js
@@ -4,19 +4,28 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Ensure this is set in your .env file
   withCredentials: true, // Allow cookies to be sent with requests
 });
+axios.defaults.withCredentials = true;
 
-api.interceptors.request.use((config) => {
-  // Retrieve CSRF token from cookies
-  const csrfToken = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("csrf_access_token="))
-    ?.split("=")[1];
+api.interceptors.request.use(
+  (config) => {
+    // Retrieve CSRF token from cookies
+    const csrfToken = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("csrf_access_token="))
+      ?.split("=")[1];
 
-  if (csrfToken) {
-    config.headers["X-CSRF-TOKEN"] = csrfToken;
+    if (csrfToken) {
+      config.headers["X-CSRF-TOKEN"] = csrfToken;
+      console.log("CSRF Token attached:", csrfToken); // Debugging log
+    } else {
+      console.error("CSRF Token not found in cookies"); // Debugging log
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 export default api;
